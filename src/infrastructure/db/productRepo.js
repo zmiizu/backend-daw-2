2049650097c7e4518fd93db4f28dbb7e8c2cd55e
zmiizu <zmiizu@outlook.com>
2026-04-28 23:24:21 +0200

    fix: comments added

--- src/infrastructure/db/productRepo.js
+++ src/infrastructure/db/productRepo.js
@@ -5,15 +5,19 @@ const { Product } = require("../../domain/Product");
 const toProduct = (r) => new Product(r);
 
 module.exports = {
+    //buscar por ID
     findById: (id) =>
         db.product.findUnique({ where: { id } }).then(r => r ? toProduct(r) : null),
 
+    //buscar por el ID del usuario
     findByUser: (userId) =>
         db.product.findMany({ where: { userId } }).then(rows => rows.map(toProduct)),
 
+    //Buscar alquileres activos
     findAllActive: () =>
         db.product.findMany({ where: { returnedAt: null } }).then(rows => rows.map(toProduct)),
 
+    //crear un producto en la bd
     create: (product) => db.product.create({
         data: {
             id: product.id, movieId: product.movieId, userId: product.userId, movieTitle: product.movieTitle,
@@ -21,6 +25,7 @@ module.exports = {
         },
     }),
 
+    //modificar cuando se devuelve el alquiler
     update: (product) => db.product.update({
         where: { id: product.id },
         data: {
